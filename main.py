d60f50bb7d434c6540f7d0ff1249ab0d1e004e55
MAKASiNer <66488841+MAKASiNer@users.noreply.github.com>
2021-02-16 23:05:56 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 from game import Game
 
+
+import os
 import pygame
+import datetime
 
 
 
 if __name__ == '__main__':
     game = Game((20, 20), 4, (1800, 800))
-    game.generateMapV2()
+    game.generateMap()
     
     pygame.init()
     pygame.display.set_caption('ⒹⓊⓇⓀⒶ')
     screen = pygame.display.set_mode(game.winSize)
 
     while True:
+        time1 = datetime.datetime.now()
+        
         game.render(screen)
         game.event()
         pygame.display.flip()
+        
+        time2 = datetime.datetime.now()
+        print("\033[H\033[J")
+        print(1000000 / (time2.microsecond - time1.microsecond))  
+        
+        
     pygame.quit()
